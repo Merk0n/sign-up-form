e57e763df1cd0a206f393edd7115be9757343a97
Merk0n <127611734+Merk0n@users.noreply.github.com>
2023-09-11 14:00:38 +0200

    done

---
+++ scipt.js
@@ -0,0 +1,6 @@
+const firstname = document.getElementById("first-name");
+const lastname = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmpassword = document.getElementById("confirm-password");
+const phonenumber = document.getElementById("phone-number");
